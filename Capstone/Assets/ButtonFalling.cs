f8ced6aa785180f5d815e0a58fb14bc739951cc5
CCASSELL20 <christiancassell@icloud.com>
2024-04-20 15:25:41 -0400

    Y
    Y

--- Capstone/Assets/ButtonFalling.cs
+++ Capstone/Assets/ButtonFalling.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ButtonFalling : MonoBehaviour
 {
@@ -8,11 +9,33 @@ public class ButtonFalling : MonoBehaviour
     public float minSpeed = 1f;
     public float maxSpeed = 5f;
 
+    public int weight;
+    public int value;
     private int buttonsFallingCount = 2;
     private Button[] fallingButtons;
 
+    public Text timeText;
+    public Text weightText;
+    public Text valueText;
+    public GameObject storageVar;
+    public GameObject failText;
+
+    void Update()
+    {
+        weightText.text = "Weight: " + weight.ToString();
+        valueText.text = "Value: " + value.ToString();
+
+        if (weight > 50)
+        {
+            storageVar.SetActive(false);
+            failText.SetActive(true);
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
     void Start()
     {
+        StartCoroutine(Countdown(60));
         fallingButtons = new Button[buttonsFallingCount];
         // Add listeners to buttons
         foreach (Button button in buttons)
@@ -26,7 +49,102 @@ public class ButtonFalling : MonoBehaviour
 
     void ButtonClicked(Button button)
     {
-        Debug.Log("Button " + button.name + " clicked!");
+        button.gameObject.SetActive(false);
+        if (button.name == "1-1")
+        {
+            weight += 1;
+            value += 1 * Random.Range(1, 2); ;
+        }
+        if (button.name == "1-2")
+        {
+            weight += 1;
+            value += 2 * Random.Range(1, 2); ;
+        }
+        if (button.name == "1-3")
+        {
+            weight += 1;
+            value += 3 * Random.Range(1, 2); ;
+        }
+        if (button.name == "2-1")
+        {
+            weight += 2;
+            value += 1 * Random.Range(1, 3); ;
+        }
+        if (button.name == "2-2")
+        {
+            weight += 2;
+            value += 2 * Random.Range(1, 3); ;
+        }
+        if (button.name == "2-3")
+        {
+            weight += 2;
+            value += 3 * Random.Range(1, 3); ;
+        }
+        if (button.name == "3-1")
+        {
+            weight += 3;
+            value += 1 * Random.Range(1, 4); ;
+        }
+        if (button.name == "3-2")
+        {
+            weight += 3;
+            value += 2 * Random.Range(1, 4); ;
+        }
+        if (button.name == "3-3")
+        {
+            weight += 3;
+            value += 3 * Random.Range(1, 4); ;
+        }
+        if (button.name == "4-1")
+        {
+            weight += 4;
+            value += 1 * Random.Range(1, 5); ;
+        }
+        if (button.name == "4-2")
+        {
+            weight += 4;
+            value += 2 * Random.Range(1, 5); ;
+        }
+        if (button.name == "4-3")
+        {
+            weight += 4;
+            value += 3 * Random.Range(1, 5); ;
+        }
+        if (button.name == "5-1")
+        {
+            weight += 5;
+            value += 3 * Random.Range(1, 6); ;
+        }
+        if (button.name == "5-2")
+        {
+            weight += 5;
+            value += 2 * Random.Range(1, 6); ;
+        }
+        if (button.name == "5-3")
+        {
+            weight += 5;
+            value += 3 * Random.Range(1, 6);
+        }
+    }
+
+    private IEnumerator Countdown(float timeRemaining)
+    {
+        while (timeRemaining > 0)
+        {
+            timeText.text = "Time: " + timeRemaining.ToString();
+            yield return new WaitForSeconds(1f);
+            timeRemaining -= 1f;
+        }
+
+        // Timer is done
+        Debug.Log("Time has run out!");
+        timeRemaining = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(1f);
+        }
+        SceneManager.LoadScene("Menu");
     }
 
     IEnumerator StartFalling()
